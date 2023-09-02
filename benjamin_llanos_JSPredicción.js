ef54daf391d5aea88518e09f2afaa3e9fef578b0
Benjamin L <103331861+Bl3x4r@users.noreply.github.com>
2023-09-02 03:12:48 -0400

    Add files via upload

---
+++ benjamin_llanos_JSPredicción.js
@@ -0,0 +1,35 @@
+//Predicción 1: ¿Qué indicará console.log cuando se llame a esta función?
+function myBirthYearFunc() {
+    console.log("Nací en " + 1980);
+}
+
+/*Estoy seguro que de llamar a la función,
+lo que se mostrará por pantalla va a ser "Nací en 1980" */
+
+
+/* Predicción 2: Si enviaramos un año de nacimiento a la función
+con la variable EntradaAñoNacimiento siendo de valor 1980
+¿Que indicaría console.log? */
+function myBirthYearFunc(EntradaAñoNacimiento) {
+    console.log("Nací en " + EntradaAñoNacimiento + "  por segunda vez");
+}
+// Estoy seguro de que console.log indicará el string "Nací en 1980"
+
+
+/*Si var num1=10 y var num2=20.
+¿Qué indicaría console.log?*/
+
+function add(num1, num2) {
+    console.log("¡Sumando números!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
+/*Mi predicción es que se indicaría la suma de ambas variables,por ende,
+en pantalla se vería el numero 30*/
+
+
+/* Solo como inciso me gustaria recalcar que despues de hacer las predicciones corrio el codigo 
+Pero no lo pongo con las modificaciones en este archivo, debido a que se llama dos veces a la misma función,
+lo cual hace que en la primera vez el valor de 1980 se deje de lado e imprima undefined*/
